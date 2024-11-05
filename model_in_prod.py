17c41f408d37152ca1db21027c7f33c7fb4e8da6
Learningisrunning <tom.dumerle@gmail.com>
2024-11-05 10:29:10 +0100

    vf pipeline train and predict

--- model_in_prod.py
+++ model_in_prod.py
@@ -5,10 +5,24 @@ import spacy
 import re
 from collections import Counter
 from kedro.pipeline import Pipeline, node
-#Data Cleaning: 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+import mlflow
+import mlflow.sklearn
+from mlflow.pyfunc import load_model
+from sklearn.metrics import hamming_loss
+from sklearn.metrics import f1_score
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import recall_score
+from sklearn.preprocessing import MultiLabelBinarizer
 
-#Data getting:
-df = pd.read_csv("data_from_stackoverflow.csv", sep=",")
+
+#Data Spliting: 
+def split_data(df):
+    df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+    return df_train, df_test
+#Data Cleaning: 
 
 # Ignorer les erreurs de certificat
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -92,7 +106,7 @@ def get_the_complit_stop_word_list(df, liste_of_custom_stopwords):
 
 #Fonction de réduction de dimension des tags
 
-def tag_dimension_reduction(colonne_tags, freq_min): 
+def tag_dimension_reduction(df, colonne_tags, freq_min): 
     all_tags = df[colonne_tags].sum()
     words_frequences = Counter(all_tags)
     liste_tags_to_keep = {mot for mot, freq in words_frequences.items() if freq >=freq_min}
@@ -126,24 +140,188 @@ def run_pipeline_data_cleaning(url) -> pd.DataFrame:
 
     return df
 
+# Mise en place du BoW 
+
+class BoWTransformer:
+    def __init__(self, max_features=1000):
+        self.max_features = max_features
+        self.vectorizer = CountVectorizer(max_features=self.max_features)
+        
+    def fit(self, Title):
+        """Ajuste le vectorizer sur les données d'entraînement."""
+        self.vectorizer.fit(Title)
+        return self
+
+    def transform(self, Title_body):
+        """Transforme les données en BoW."""
+        return self.vectorizer.transform(Title_body)
+
+    def to_dataframe(self, data_transform):
+        """Ajuste et transforme les données d'entraînement."""
+        return pd.DataFrame(data_transform.toarray(),columns=self.vectorizer.get_feature_names_out())
+
+# Fonction de prétraitement pour créer des colonnes tokenisées
+def preprocess_for_bow(df):
+    df["Title_tokenized_for_BoW"] = df["Title_tokenized"].apply(lambda x: ' '.join(x))
+    df["Title_and_body_tokenized_for_BoW"] = (
+        df["Title_tokenized"].apply(lambda x: ' '.join(x)) + 
+        df["Body_tokenized"].apply(lambda x: ' '.join(x))
+    )
+    return df
+
+def last_processing_for_BoW(df, save_name):
+    df["tags"] = df["tags_liste"]
+    df = df.dropna()
+    df.to_csv(save_name, index=False)
+
+
+
+def pipeline_transformation_to_BoW(df, df_name):
+
+    df_boW = preprocess_for_bow(df)
+
+    BoW_traitement = BoWTransformer(max_features=1000)
+
+    X_fit = BoW_traitement.fit(df_boW["Title_tokenized_for_BoW"])
+
+    X_transform = BoW_traitement.transform(df_boW["Title_and_body_tokenized_for_BoW"])
+
+    df_boW_vf = BoW_traitement.to_dataframe(X_transform)
+
+    last_processing_for_BoW(df_boW_vf, df_name) 
+    
+
+#Mise en place du ML:
+
+def MultinomialNB_BoW_train(df_train):
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+    mlflow.start_run()
+
+    X_train = df_train.drop("tags", axis = 1)
+    y_train = df_train["tags"]
+
+    pipeline_multinomialNB = Pipeline([
+        ('model', MultinomialNB())
+        ])
+
+    pipeline_multinomialNB.fit(X_train, y_train)
+
+
+    mlflow.sklearn.log_model(pipeline_multinomialNB, "MultinomialNB_BoW")
+
+    model_name = "MultinomialNB_BoW"
+    run_id = mlflow.active_run().info.run_id  # Utiliser le run_id de l'expérience en cours
+
+    # Ajout dans le Model Registry
+    mlflow.register_model(f"runs:/{run_id}/MultinomialNB_BoW", model_name)
+
+    return pipeline_multinomialNB
+
+
+
+def MultinomialNB_BoW_predict(df_test):
+
+    X_test = df_test.drop("tags", axis = 1)
+    y_test = df_test["tags"]
+    model_name = "MultinomialNB_BoW"
+    model_uri = f"models:/{model_name}/latest"
+    pipeline_multinomialNB = load_model(model_uri)
+    # Prédiction
+    y_pred = pipeline_multinomialNB.predict(X_test)
+
+    return y_pred, y_test
+
+def accuracy(y_pred, y_test):
+    # Binarisation des données
+    mlb = MultiLabelBinarizer()
+    y_test_bin = mlb.fit_transform(y_test)
+    y_pred_bin = mlb.transform(y_pred)
+
+    hamming_score = hamming_loss(y_test_bin, y_pred_bin)
+    f1_micro_score = f1_score(y_test_bin, y_pred_bin, average="micro")
+    f1_macro_score = f1_score(y_test_bin, y_pred_bin, average="macro")
+    # Calcul du Jaccard score avec average='samples'
+    jaccard_Score = jaccard_score(y_test_bin, y_pred_bin, average='samples')
+
+
+    #Méthode d'évaluation de couverture des tags:
+
+    recall_macro_score = recall_score(y_test_bin, y_pred_bin, average='macro')
+
+
+    data = {
+        "Hamming Loss" : hamming_score,
+        "f1 macro" : f1_macro_score,
+        "f1 micro" : f1_micro_score,
+        "jaccard" : jaccard_Score,
+        "recall macro" : recall_macro_score
+    }
+
+    data_score = pd.DataFrame(data, index=[0])
+
+    return data_score
 
 
 
 
-def create_pipeline(**kwargs) -> Pipeline:
+def training_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
         [
             node(
                 func=load_data,
-                inputs=None,  # Pas d'entrée ici car on charge directement le fichier
-                outputs="raw_data",  # Sortie à utiliser par la suite
+                inputs=None,  
+                outputs="raw_data",  
                 name="load_data_node",
             ),
             node(
+                func=split_data,
+                inputs="raw_data",
+                outputs=["df_train", "df_test"],
+                name="split_data_node",
+            ),
+            node(
                 func=run_pipeline_data_cleaning,
-                inputs="raw_data",  # Utilise le DataFrame chargé par le premier node
-                outputs="processed_data",  # Sortie du traitement
-                name="data_processing_node",
+                inputs="df_train",  
+                outputs="processed_data_train",  
+                name="data_cleaning_train_node",
+            ),
+            node(
+                func=pipeline_transformation_to_BoW,
+                inputs="processed_data_train",
+                outputs="processed_data_boW_train",
+                name="BoW_transformation_train_node",
+            ),
+            node(
+                func=train_model,  # Fonction d'entraînement du modèle
+                inputs="processed_data_boW_train",  
+                outputs="trained_model",
+                name="train_model_node",
+            ),
+        ]
+    )
+
+
+def prediction_pipeline(**kwargs) -> Pipeline:
+    return Pipeline(
+        [
+            node(
+                func=run_pipeline_data_cleaning,
+                inputs="df_test",
+                outputs="processed_data_test",
+                name="data_cleaning_test_node",
+            ),
+            node(
+                func=pipeline_transformation_to_BoW,
+                inputs="processed_data_test",
+                outputs="processed_data_boW_test",
+                name="BoW_transformation_test_node",
+            ),
+            node(
+                func=predict_model,  # Fonction pour faire les prédictions
+                inputs=["trained_model", "processed_data_boW_test"],
+                outputs="predictions",
+                name="predict_model_node",
             ),
         ]
     )
