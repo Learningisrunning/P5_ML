c795e2fdd490332d485088468af8ae088ecbcec3
Learningisrunning <tom.dumerle@gmail.com>
2024-12-10 16:00:24 +0100

    change in config cotalog

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Lancer FastAPI en arrière-plan
-nohup uvicorn API.app:app --host=0.0.0.0 --port=8000 &
-
-# Lancer Streamlit
-streamlit run stream_lite.app_streamlit.py --server.port=${PORT:-8501} --server.enableCORS=false
