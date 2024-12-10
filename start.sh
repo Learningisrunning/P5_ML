4d997397f6f0e29c86b1cd454de976c1d0f9e1ed
Learningisrunning <tom.dumerle@gmail.com>
2024-12-10 15:27:03 +0100

    change in start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Lancer FastAPI en arrière-plan
-uvicorn fastapi_app.main:app --host=0.0.0.0 --port=8000 &
+nohup uvicorn API.app:app --host=0.0.0.0 --port=8000 &
 
 # Lancer Streamlit
-streamlit run streamlit_app/app.py --server.port=${PORT} --server.enableCORS=false
+streamlit run stream_lite.app_streamlit.py --server.port=${PORT:-8501} --server.enableCORS=false
